bd8c17937ffba629be5f778cb728b1a97e24ae6e
Gibberditoslumber <91945956+Gibberditoslumber@users.noreply.github.com>
2022-03-16 00:32:02 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-git clone https://github.com/STJr/Kart-Public.git && cd Kart-Public && make -C src/ LINUX64=1
+#!/bin/bash
+
+until [[ ${ANSWER,,} == "32bit" ]] || [[ ${ANSWER,,} == "64bit" ]]; do
+	read -p "if you're pc is 64 or 32 bit if 64 bit then type 64bit if 32 bit then type 32bit " ANSWER
+done
+
+if [[ ${ANSWER,,} == "64bit" ]]; then
+sudo apt-get install build-essential libpng-dev zlib1g-dev libsdl2-dev libsdl2-mixer-dev libgme-dev libcurl4-openssl-dev && git clone https://github.com/STJr/Kart-Public.git && cd Kart-Public && make -C src/ LINUX64=1
+else
+	sudo apt-get install build-essential libpng-dev zlib1g-dev libsdl2-dev libsdl2-mixer-dev libgme-dev libopenmpt-dev libcurl4-openssl-dev nasm && git clone https://github.com/STJr/Kart-Public.git && cd Kart-Public && make -C src/ LINUX=1
